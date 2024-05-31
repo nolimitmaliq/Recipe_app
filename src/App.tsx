e39e402acf9a2bd5a212f29e4ea5f59bb425d9f9
Maliq Adewale <maliqade@udel.edu>
2024-05-31 08:00:26 -0400

    environment setup

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import { Button, Form } from 'react-bootstrap';
+
+
+function App() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src= "https://shorturl.at/NASJH" className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+
+  export default App;
+  
