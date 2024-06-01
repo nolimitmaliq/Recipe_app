5d5a9a23b12cb7fae9446fd6ba4a10959a89cfb5
Maliq Adewale <maliqade@udel.edu>
2024-05-31 21:57:29 -0400

    designing home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,13 @@ import React, { useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import { Button, Form } from 'react-bootstrap';
+import {Homepage} from './HomePage'
 
 
 function App() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src= "https://shorturl.at/NASJH" className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Homepage />
       </div>
     );
   }
---
+++ src/HomePage.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import './App.css';
+import { Button, Form } from 'react-bootstrap';
+// import image1 from './Images/image1.jpg';
+// import image2 from "./Images/image2.jpg";
+// import image3 from "./Images/image3.jpg";
+// import image4 from "./Images/image4.jpg";
+// import image5 from "./Images/image5.jpg";
+// import image6 from "./Images/image6.jpg";
+// import image7 from "./Images/image7.jpg";
+// import image8 from "./Images/image8.jpg";
+// import image9 from "./Images/image9.jpg";
+
+export function Homepage(): JSX.Element{
+    return (
+        <div className = "Section_top">
+            <div className = "content">
+                hello
+            </div>
+        </div>
+    )
+}
