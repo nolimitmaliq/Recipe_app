08c43304119c54906ca3a61475706a04cbe423b6
Maliq Adewale <maliqade@udel.edu>
2024-07-05 00:20:08 -0400

    more minor changes

--- src/Breakfast.tsx
+++ src/Breakfast.tsx
@@ -25,8 +25,29 @@ export function BreakFast(): JSX.Element{
                 </Form>
                 <button>Submit Recipe</button>
             </div>
-            <div>Culinary Advice
-                <h3>Optimize your experience with our design</h3>
+            <div className = "Advice">
+                <h3>Culinary Advice</h3>
+                <h5>Optimize your experience with our design</h5>
+                <ul>
+                    <li><span>Be Specific with Ingredients</span>
+                        :List all the ingredients you have on hand to get the most accurate recipe suggestions.
+                        Include quantities if possible to ensure better matching.
+                    </li>
+                    <li><span>Dietary Preference</span>
+                        :Mention any dietary restrictions or preferences (e.g., vegetarian, gluten-free, low-carb) 
+                        to receive recipes that fit your needs.
+                    </li>
+                    <li><span>Explore Different Cuisines</span>
+                        {'    '}:Use the cuisine filter to discover new and exciting recipes from different cultures and regions.
+                    </li>
+                    <li><span>Adjust Serving Sizes</span>
+                        :Use the serving size feature to scale recipes up 
+                        or down based on the number of people you are cooking for. 
+                    </li>
+                    <li><span>Save Your Favorites:</span>
+                        :Bookmark or save your favorite recipes for easy access and future reference.
+                    </li>    
+                </ul>
             </div>
         </div>
     )
