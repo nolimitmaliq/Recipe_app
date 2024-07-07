6894add596426222e1683f5163f5ca5b57b841b7
Maliq Adewale <maliqade@udel.edu>
2024-07-07 17:54:48 -0400

    more minor changes

--- src/Breakfast.tsx
+++ src/Breakfast.tsx
@@ -6,7 +6,7 @@ export function BreakFast(): JSX.Element{
     const [tab, setTab] = useState<string>("home");
     return (
         <div>
-            <div className = "content">
+            <div className = "content1">
                 <h1>Generate A Breakfast Meal</h1>
                 <Form>
                     <Form.Group controlId='Breakfast'></Form.Group>
@@ -26,28 +26,28 @@ export function BreakFast(): JSX.Element{
                 <button>Submit Recipe</button>
             </div>
             <div className = "Advice">
-                <h3>Culinary Advice</h3>
-                <h5>Optimize your experience with our design</h5>
-                <ul>
-                    <li><span>Be Specific with Ingredients</span>
-                        :List all the ingredients you have on hand to get the most accurate recipe suggestions.
+                <h1>Cupnary Advice</h1>
+                <div>Optimize your experience with our design</div>
+                <div className='whiteCard'>
+                    <p><span>Be Specific with Ingredients</span>
+                        :pst all the ingredients you have on hand to get the most accurate recipe suggestions.
                         Include quantities if possible to ensure better matching.
-                    </li>
-                    <li><span>Dietary Preference</span>
+                    </p>
+                    <p><span>Dietary Preference</span>
                         :Mention any dietary restrictions or preferences (e.g., vegetarian, gluten-free, low-carb) 
                         to receive recipes that fit your needs.
-                    </li>
-                    <li><span>Explore Different Cuisines</span>
+                    </p>
+                    <p><span>Explore Different Cuisines</span>
                         {'    '}:Use the cuisine filter to discover new and exciting recipes from different cultures and regions.
-                    </li>
-                    <li><span>Adjust Serving Sizes</span>
+                    </p>
+                    <p><span>Adjust Serving Sizes</span>
                         :Use the serving size feature to scale recipes up 
                         or down based on the number of people you are cooking for. 
-                    </li>
-                    <li><span>Save Your Favorites:</span>
+                    </p>
+                    <p><span>Save Your Favorites</span>
                         :Bookmark or save your favorite recipes for easy access and future reference.
-                    </li>    
-                </ul>
+                    </p>    
+                </div>
             </div>
         </div>
     )
